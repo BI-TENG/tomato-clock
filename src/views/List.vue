6f7ade951342e250041a65306a9b98aa7e7ef190
BI-TENG <SHIEBT.1026@gmail.com>
2020-05-26 13:17:15 +0800

    list 新增標題欄位、判斷式

--- src/views/List.vue
+++ src/views/List.vue
@@ -3,8 +3,17 @@
     <b-form-input v-model="newtodo"></b-form-input>
     <b-btn variant="success" @click="addTodo">新增</b-btn>
     <b-table-simple>
+      <b-thead>
+        <b-tr>
+          <b-th>事項</b-th>
+          <b-th>動作</b-th>
+        </b-tr>
+      </b-thead>
       <draggable v-model="todos" tag="tbody" v-bind="dragOption">
-        <b-tr v-for="(todo,index) in todos" :key="index">
+        <b-tr v-if="todos.length == 0">
+          <b-td colspan="2">沒有資料</b-td>
+        </b-tr>
+        <b-tr v-else v-for="(todo,index) in todos" :key="index">
           <b-td>{{todo.name}}</b-td>
           <b-td></b-td>
         </b-tr>
