be39309686d779a4f7e82b06f611cae6e46886d7
BI-TENG <SHIEBT.1026@gmail.com>
2020-05-26 10:44:55 +0800

    設定鬧鈴鈴聲頁面

---
+++ src/views/Settings.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="settings">
+    <b-table :items="items" :fields="fields">
+      <template v-slot:cell(preview)="data">
+        <audio controls :src="'./alarms/'+data.item.file"></audio>
+      </template>
+    </b-table>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      items: [
+        {
+          name: '鬧鈴',
+          file: 'alarm1.mp3'
+        },
+        {
+          name: '鬧鈴2',
+          file: 'alarm2.mp3'
+        }
+      ],
+      fields: [
+        {
+          key: 'name',
+          label: '名稱'
+        },
+        {
+          key: 'preview',
+          label: '預覽'
+        },
+        {
+          key: 'select',
+          label: '選擇'
+        }
+      ]
+    }
+  }
+}
+</script>
