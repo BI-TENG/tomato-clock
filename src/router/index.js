5c424c95e38c6af7f35a3f192387c34458eed574
BI-TENG <SHIEBT.1026@gmail.com>
2020-05-26 09:33:49 +0800

    設定網頁標籤 title

--- src/router/index.js
+++ src/router/index.js
@@ -8,7 +8,10 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta: {
+      title: '番茄鐘'
+    }
   },
   {
     path: '/list',
@@ -16,7 +19,10 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/List.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/List.vue'),
+    meta: {
+      title: '番茄鐘 | 清單'
+    }
   },
   {
     path: '/settings',
@@ -24,12 +30,19 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Settings.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/Settings.vue'),
+    meta: {
+      title: '番茄鐘 | 設定'
+    }
   }
 ]
 
 const router = new VueRouter({
   routes
+})
+
+router.afterEach((to, from) => {
+  document.title = to.meta.title
 })
 
 export default router
