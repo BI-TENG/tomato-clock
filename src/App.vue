25929ed9217329a5cba18fcf18bc3807555c9ed6
BI-TENG <SHIEBT.1026@gmail.com>
2020-05-26 10:27:05 +0800

    設定頁面

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/list">清單</router-link> |
+      <router-link to="/settings">設定</router-link> |
     </div>
     <router-view/>
   </div>
