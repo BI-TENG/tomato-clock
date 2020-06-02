9575305fc56a84f42b05eaba19a0b59e744d1edf
BI-TENG <SHIEBT.1026@gmail.com>
2020-06-02 18:33:02 +0800

    更改目錄樣式

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import store from './store'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faCheck, faPen, faTimes, faUndo, faSave, faPlay, faPause, faStepForward } from '@fortawesome/free-solid-svg-icons'
+import { faCheck, faPen, faTimes, faUndo, faSave, faPlay, faPause, faStepForward, faHome, faMusic, faListUl } from '@fortawesome/free-solid-svg-icons'
 
 import draggable from 'vuedraggable'
 import RadialProgressBar from 'vue-radial-progress'
@@ -15,10 +15,15 @@ import RadialProgressBar from 'vue-radial-progress'
 import 'noto-sans-tc/noto_sans_tc_regular/css.css'
 import './style/style.styl'
 import CircleMenu from 'vue-circle-menu'
+import VueGtag from 'vue-gtag'
+
+Vue.use(VueGtag, {
+  config: { id: 'UA-1234567-1' }
+})
 
 Vue.config.productionTip = false
 
-library.add(faCheck, faPen, faTimes, faUndo, faSave, faPlay, faPause, faStepForward)
+library.add(faCheck, faPen, faTimes, faUndo, faSave, faPlay, faPause, faStepForward, faHome, faMusic, faListUl)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.component('draggable', draggable)
